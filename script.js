94a3d6252de1e4e5977e700c87841649fe3c5446
sandeepkavinda45 <sandeepkavinda203@gmail.com>
2022-05-19 16:23:23 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,168 @@
+// ======= Welcome Text Animation =======
+
+var x = document.getElementById("da1");
+
+function welcomeTextScaleUp() {
+    x.style.transform = "scale(1.1)";
+}
+
+function welcomeTextScaleDown() {
+    x.style.transform = "scale(1)";
+}
+
+// ======= Welcome Text BackGround Animation =======
+
+function welcomeTextBgChange() {
+    document.getElementById("da").style.backgroundColor = "rgba(0, 0, 0, 0.75)";
+
+}
+
+function welcomeTextBgNormal() {
+    document.getElementById("da").style.backgroundColor = "rgba(0, 0, 0, 0.65)";
+
+}
+
+// ======= Paragraphs Animation =======
+
+// Pragraph 1
+function p1Enter() {
+    document.getElementById("p1").style.transform = "scale(0.95)";
+}
+
+function p1Leave() {
+    document.getElementById("p1").style.transform = "scale(1)";
+}
+
+// Pragraph 2
+function p2Enter() {
+    document.getElementById("p2").style.transform = "scale(0.95)";
+}
+
+function p2Leave() {
+    document.getElementById("p2").style.transform = "scale(1)";
+}
+
+// Pragraph 3
+function p3Enter() {
+    document.getElementById("p3").style.transform = "scale(0.95)";
+}
+
+function p3Leave() {
+    document.getElementById("p3").style.transform = "scale(1)";
+}
+
+// Pragraph 4
+function p4Enter() {
+    document.getElementById("p4").style.transform = "scale(0.95)";
+}
+
+function p4Leave() {
+    document.getElementById("p4").style.transform = "scale(1)";
+}
+
+// Pragraph 5
+function p5Enter() {
+    document.getElementById("p5").style.transform = "scale(0.95)";
+}
+
+function p5Leave() {
+    document.getElementById("p5").style.transform = "scale(1)";
+}
+
+
+
+
+// ======= Cards Animation =======
+
+// Card 1
+function c1Enter() {
+    document.getElementById("c1").style.transform = "scale(1.1)";
+}
+
+function c1Leave() {
+    document.getElementById("c1").style.transform = "scale(1)";
+}
+
+// Card 2
+function c2Enter() {
+    document.getElementById("c2").style.transform = "scale(1.1)";
+}
+
+function c2Leave() {
+    document.getElementById("c2").style.transform = "scale(1)";
+}
+
+// Card 3
+function c3Enter() {
+    document.getElementById("c3").style.transform = "scale(1.1)";
+}
+
+function c3Leave() {
+    document.getElementById("c3").style.transform = "scale(1)";
+}
+
+// Card 4
+function c4Enter() {
+    document.getElementById("c4").style.transform = "scale(1.1)";
+}
+
+function c4Leave() {
+    document.getElementById("c4").style.transform = "scale(1)";
+}
+
+// Card 5
+function c5Enter() {
+    document.getElementById("c5").style.transform = "scale(1.1)";
+}
+
+function c5Leave() {
+    document.getElementById("c5").style.transform = "scale(1)";
+}
+
+
+// ======= Footer Icons Animation =======
+// Logo 1
+function fl1Enter() {
+    document.getElementById("fl1").style.transform = "scale(1.1)";
+}
+
+function fl1Leave() {
+    document.getElementById("fl1").style.transform = "scale(1)";
+}
+
+
+// Icon 1
+function i1Enter() {
+    document.getElementById("i1").style.transform = "scale(1.1)";
+}
+
+function i1Leave() {
+    document.getElementById("i1").style.transform = "scale(1)";
+}
+
+// Icon 2
+function i2Enter() {
+    document.getElementById("i2").style.transform = "scale(1.1)";
+}
+
+function i2Leave() {
+    document.getElementById("i2").style.transform = "scale(1)";
+}
+
+// Icon 3
+function i3Enter() {
+    document.getElementById("i3").style.transform = "scale(1.1)";
+}
+
+function i3Leave() {
+    document.getElementById("i3").style.transform = "scale(1)";
+}
+
+// Icon 4
+function i4Enter() {
+    document.getElementById("i4").style.transform = "scale(1.1)";
+}
+
+function i4Leave() {
+    document.getElementById("i4").style.transform = "scale(1)";
+}
